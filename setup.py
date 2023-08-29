10b6c762066b9817f24166ddfe62a7406c8a671f
mmcentre <michael.bouzinier@kvitneset.com>
2023-08-28 20:56:58 -0400

    MAB           Update setup.py to account for internal dependencies

--- setup.py
+++ setup.py
@@ -39,7 +39,8 @@ setup(
         'requests',
         'rioxarray',
         'xarray',
-        'nsaph_gis>=0.0.3',
+        'nsaph @ git+https://github.com/NSAPH-Data-Platform/nsaph-core-platform.git@develop',
+        'nsaph_gis @ git+https://github.com/NSAPH-Data-Platform/nsaph-gis.git@develop'
     ],
     package_data={
         '': ["**/*.yaml"]
