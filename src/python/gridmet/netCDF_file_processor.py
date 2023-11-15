a16c4e8cb64084f3fb928b25a00e773a5466dcc6
mmcentre <michael.bouzinier@kvitneset.com>
2023-11-14 22:33:29 -0500

    MAB    Making the step to ingest exposure data into Dorieh optional

--- src/python/gridmet/netCDF_file_processor.py
+++ src/python/gridmet/netCDF_file_processor.py
@@ -1,3 +1,34 @@
+
+#  Copyright (c) 2023.  Harvard University
+#
+#   Developed by Research Software Engineering,
+#   Harvard University Research Computing and Data (RCD) Services.
+#
+#   Author: Michael A Bouzinier
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#          http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#
+
+"""
+An entry point to a command line utility aggregating grid data
+provided as NetCDF file over a set of shape files, assigning
+labels defined in the shape files to the aggregated values
+
+
+`see https://www.unidata.ucar.edu/software/netcdf/`__
+"""
+
 import logging
 import os
 from typing import Optional
@@ -10,16 +41,6 @@ from nsaph_gis.compute_shape import StatsCounter
 
 from gridmet.config import GridContext, OutputType
 from gridmet.aggregator import Aggregator, GeoTiffAggregator, NetCDFAggregator
-
-"""
-An entry point to a command line utility aggregating grid data
-provided as NetCDF file over a set of shape files, assigning
-labels defined in the shape files to teh aggregated values
-
-
-`see https://www.unidata.ucar.edu/software/netcdf/`__ 
-"""
-
 
 class NetCDFFile:
     def __init__(self, context: GridContext = None):
--- src/python/pollution/wustl_file_processor.py
+++ src/python/pollution/wustl_file_processor.py
@@ -1,3 +1,36 @@
+#  Copyright (c) 2023.  Harvard University
+#
+#   Developed by Research Software Engineering,
+#   Harvard University Research Computing and Data (RCD) Services.
+#
+#   Author: Michael A Bouzinier
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#          http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#
+
+"""
+An entry point to aggregating grid data produced by
+Atmospheric Composition Analysis Group of Washington
+University in St. Louis.
+
+The tool validates file names that they are using the naming pattern
+employed by the group and infers year and month of the observations
+from the file name.
+
+`See data description: <https://sites.wustl.edu/acag/datasets/surface-pm2-5/>`_
+"""
+
 import re
 
 from gridmet.netCDF_file_processor import NetCDFFile
